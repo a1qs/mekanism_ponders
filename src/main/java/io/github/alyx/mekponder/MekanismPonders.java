afd84e609e9185dd902d65e80e9f587e9d294a44
a1qs <ankiller2007.09.20@gmail.com>
2026-01-06 13:54:03 +0100

    chore: boolean field to check whether Mekanism: Generators is loaded

--- src/main/java/io/github/alyx/mekponder/MekanismPonders.java
+++ src/main/java/io/github/alyx/mekponder/MekanismPonders.java
@@ -1,6 +1,7 @@
 package io.github.alyx.mekponder;
 
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.fml.ModList;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -14,12 +15,17 @@ import net.neoforged.fml.ModContainer;
 // TODO: Replace "the user" -> "You"
 // TODO: Figure out Energy Cube and transmitter logic
 // TODO: mention UI in Dynamic tank
+// TODO: Improve multi particles
+// TODO: Seperate Stuff from MekGens and only load if mekgens is loaded (e.g reactor glass, heat generator)
 @Mod(MekanismPonders.MODID)
 public class MekanismPonders {
     public static final String MODID = "mekanism_ponders";
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    public static boolean isMekGensLoaded = false;
+
     public MekanismPonders(IEventBus modEventBus, ModContainer modContainer) {
+        isMekGensLoaded = ModList.get().isLoaded("mekanismgenerators"); // We only want to add Ponders that include MekanismGenerators things if the mod is loaded.
     }
 
     public static ResourceLocation id(String name) {
